679b0c5b8cde475bd927343a9111c3949234ece4
Nicholas Herrick <nth.herrick@gmail.com>
2020-06-17 14:08:03 -0600

    removed unused redirect from App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import history from './utils/history';
 import Home from './views/Home';
 import './scss/App.scss';
